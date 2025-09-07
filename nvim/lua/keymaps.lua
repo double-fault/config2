52ae2b1f8dfb5c365309991a58fc4eff367c8a4e
Ashish Ahuja <shivashish.ahuja@gmail.com>
2025-09-07 11:41:54 +0530

    add alacritty stuff

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,6 +4,13 @@ vim.keymap.set("n", "<leader>rc", ":luafile %<CR>", { desc = "Reload neovim conf
 vim.keymap.set("n", "<C-g>", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", { desc = "Focus file explorer" })
 
+-- Yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", '"+y', { desc = "[y]ank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "[Y]ank current line to system clipboard" })
+
+-- Paste from system clipboard
+vim.keymap.set({"n", "v"}, "<leader>p", '"+p', { desc = "[P]aste from system clipboard" })
+
 --
 -- Telescope keymaps
 local builtin = require 'telescope.builtin'
@@ -11,3 +18,7 @@ local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>sn', function()
     builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim config' })
+
+vim.keymap.set('n', '<leader>sa', function()
+    builtin.find_files { cwd = vim.fn.stdpath('config') .. '/../alacritty' }
+end, { desc = '[S]earch [A]lacritty config' })
